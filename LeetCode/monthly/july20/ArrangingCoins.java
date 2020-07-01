0081ecd7702dba5e1f96bc80741401445c823821
Rohit Singhal <irohitsinghal@localhost>
2020-07-01 21:21:46 +0530

    Updated the algo for ArrangingCoins to constant time

--- LeetCode/monthly/july20/ArrangingCoins.java
+++ LeetCode/monthly/july20/ArrangingCoins.java
@@ -3,19 +3,18 @@ package LeetCode.monthly.july20;
 public class ArrangingCoins {
 
     public int arrangeCoins(int n) {
-        int k = 1, count = 0;
+        // We can use sum of n natural numbers method n(n+1)/2
+        // and use Quadratic equation a*n^2 + b*n + c
+        long a = 1, b = 1, c = -2L * n;
 
-        while (n - k >= 0) {
-            n -= k;
-            k++;
-            count++;
-        }
+        double x = Math.sqrt(b * b - 4 * a * c);
+        double val = (-1 * b + x) / 2 * a;
 
-        return count;
+        return (int) val;
     }
 
     public static void main(String[] args) {
-        int coins = 8;
+        int coins = 1804289383;
 
         System.out.println(new ArrangingCoins().arrangeCoins(coins));
     }
